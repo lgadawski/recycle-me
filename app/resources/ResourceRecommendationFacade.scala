99468468312709d920995ce98f961009f5305182
Lukasz Gadawski <l.gadawski@gmail.com>
2018-02-27 14:32:06 +0100

    Price encapsulation.

--- app/resources/ResourceRecommendationFacade.scala
+++ app/resources/ResourceRecommendationFacade.scala
@@ -1,6 +1,7 @@
 package resources
 
 import javax.inject.Inject
+import price.Price
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -12,7 +13,7 @@ trait ResourceRecommendationFacade {
     *
     * @return collection of address id and aggregated resource cost in that address
     */
-  def recommendCityList(collectorId: Int): Future[Seq[(Int, BigDecimal)]]
+  def recommendCityList(collectorId: Int): Future[Seq[(Int, Price)]]
 
   /**
     * Recommend resources for given collector that are available in given location. Sort list by best
@@ -20,33 +21,33 @@ trait ResourceRecommendationFacade {
     *
     * @return collection of resource id and corresponding cost
     */
-  def recommendResources(collectorId: Int, cityId: Int): Future[Seq[(Int, BigDecimal)]]
+  def recommendResources(collectorId: Int, cityId: Int): Future[Seq[(Int, Price)]]
 
 }
 
 class ResourceRecommendationFacadeImpl @Inject()(resourceRepository: ResourceRepository)
                                                 (implicit ex: ExecutionContext) extends ResourceRecommendationFacade {
 
-  override def recommendResources(collectorId: Int, cityId: Int): Future[Seq[(Int, BigDecimal)]] = {
+  override def recommendResources(collectorId: Int, cityId: Int): Future[Seq[(Int, Price)]] = {
     val resources: Future[Seq[(Int, String, Int)]] = resourceRepository.getByCollectorByCity(collectorId, cityId)
-
+val x: Int = null
     resources.map {
       _
-        .map(a => (a._1, BigDecimal(a._2).*(a._3)))
-        .sortBy(-_._2)
+        .map(a => (a._1, Price(BigDecimal(a._2)).multiply(a._3)))
+        .sortBy(_._2)(Ordering[Price].reverse)
     }
   }
 
-  override def recommendCityList(collectorId: Int): Future[Seq[(Int, BigDecimal)]] = {
+  override def recommendCityList(collectorId: Int): Future[Seq[(Int, Price)]] = {
     val resources: Future[Seq[(Int, String, Int)]] = resourceRepository.getByCollector(collectorId)
 
     resources.map {
       _
         .groupBy(_._1)
-        .mapValues(_.map(e => BigDecimal(e._2).*(e._3)).sum)
+        .mapValues(_.map(e => Price(BigDecimal(e._2)).multiply(e._3)).sum)
         .map(a => (a._1, a._2))
         .toSeq
-        .sortBy(-_._2)
+        .sortBy(_._2)(Ordering[Price].reverse)
     }
   }
 }
